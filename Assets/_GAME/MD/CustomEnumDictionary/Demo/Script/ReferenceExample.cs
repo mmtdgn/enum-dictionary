1e6f99f8515e2407ebbfead4df888b14a35e26d1
Mehmet Doğan <mmt.dgn.6634@gmail.com>
2022-08-17 23:06:24 +0300

    Clean scene & improve example code

--- Assets/_GAME/MD/CustomEnumDictionary/Demo/Script/ReferenceExample.cs
+++ Assets/_GAME/MD/CustomEnumDictionary/Demo/Script/ReferenceExample.cs
@@ -4,12 +4,28 @@ public class ReferenceExample : MonoBehaviour
 {
     [Header("The dictionary can be referenced from another class")]
     [SerializeField] protected StringColorEnumDictionary m_EnumDictionary;
+    [Space(10)]
+    [SerializeField] States m_State;
+    States m_OldState;
+    [Space(10)]
     [SerializeField] Color m_Color;
     [SerializeField] string m_String;
 
     void Start()
     {
-        m_Color = m_EnumDictionary.GetValuesFromDictionary(States.State2).Color;
-        m_String = m_EnumDictionary.GetValuesFromDictionary(States.State2).String;
+        GetData();
+    }
+
+    private void Update()
+    {
+        if (m_OldState == m_State) return;
+        GetData();
+    }
+
+    public void GetData()
+    {
+        m_Color = m_EnumDictionary.GetValuesFromDictionary(m_State).Color;
+        m_String = m_EnumDictionary.GetValuesFromDictionary(m_State).String;
+        m_OldState = m_State;
     }
 }
